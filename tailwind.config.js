e0062e00fbe59afc59cc10b98fb9f72fb4eb97d3
tenztshering <tenztshering3@gmail.com>
2023-04-02 13:34:07 +0600

    :fire: selection color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{html,js,jsx}",
-    "./components/**/*.{html,js,jsx}",
-    "./sections/**/*.{html,js,jsx}",
-    "./styles/**/*.{js,jsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{html,js,jsx}',
+    './components/**/*.{html,js,jsx}',
+    './sections/**/*.{html,js,jsx}',
+    './styles/**/*.{js,jsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
   ],
-  mode: "jit",
+  mode: 'jit',
   theme: {
     extend: {
       colors: {
-        primary: "#1E4079",
-        secondary: "#aaa6c3",
-        tertiary: "#151030",
-        "blue-100": "#71d7ff",
-        "yellow-100": "#ffd768",
-        "black-100": "#100d25",
-        "black-200": "#090325",
-        "white-100": "#f3f3f3",
+        primary: '#1E4079',
+        secondary: '#aaa6c3',
+        tertiary: '#151030',
+        'blue-100': '#71d7ff',
+        'yellow-100': '#ffd768',
+        'black-100': '#100d25',
+        'black-200': '#090325',
+        'white-100': '#f3f3f3',
       },
       boxShadow: {
-        card: "0px 35px 120px -15px #211e35",
+        card: '0px 35px 120px -15px #211e35',
       },
       screens: {
-        xs: "450px",
+        xs: '450px',
       },
       backgroundImage: {
-        "hero-pattern": "url('/herobg.png')",
+        'hero-pattern': "url('/herobg.png')",
       },
     },
   },
