13f554bcc9ca8d5685135edb22a20699dc123e79
tenztshering <tenztshering3@gmail.com>
2023-04-02 13:52:00 +0600

    :fire: added SEO

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,31 @@
 import Head from 'next/head';
+import { NextSeo } from 'next-seo';
 import '../styles/globals.css';
 import { Footer } from '../components';
 
 const MyApp = ({ Component, pageProps }) => (
   <>
-    <Head>
-      <title>BhutanNews</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+    <NextSeo
+      title="BhutanNews"
+      titleTemplate="BhutanNews"
+      defaultTitle="BhutanNews"
+      description="An app that brings all the media houses of Bhutan in one platform. Mainly to bring back news reading by making PDF subscription cheaper and accessible in modern way"
+      canonical="https://bhutannews.vercel.app/"
+      openGraph={{
+        url: 'https://bhutannews.vercel.app/',
+        title: 'BhutanNews',
+        description:
+          'An app that brings all the media houses of Bhutan in one platform. Mainly to bring back news reading by making PDF subscription cheaper and accessible in modern way',
+        images: [
+          {
+            url: '/AppIcon.png',
+            width: 800,
+            height: 420,
+            alt: 'BhutanNews',
+          },
+        ],
+      }}
+    />
     <Component {...pageProps} />
     <Footer />
   </>
