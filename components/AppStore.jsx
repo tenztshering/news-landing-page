5ed147e0511ea3b0ef80d461a67ca72d54abb39f
tenztshering <tenztshering3@gmail.com>
2023-04-02 15:23:47 +0600

    :rocket: App link

--- components/AppStore.jsx
+++ components/AppStore.jsx
@@ -3,7 +3,7 @@ import { memo } from 'react';
 
 const AppStore = () => (
   <div className="flex justify-center items-center gap-2">
-    <a href="/legal">
+    <a href="https://play.google.com/store/apps/details?id=bt.jobajoba.news.myapp&hl=en&gl=US">
       <Image
         src="/google.svg"
         alt="google play store"
@@ -14,7 +14,7 @@ const AppStore = () => (
         className="w-full h-[44px] max-sm:h-[35px] object-contain hover:opacity-50"
       />
     </a>
-    <a href="/legal">
+    <a href="https://apps.apple.com/bt/app/bhutannews/id6446471566">
       <Image
         src="/apple.svg"
         alt="google play store"
