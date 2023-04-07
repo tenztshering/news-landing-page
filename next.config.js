3d93d6c9ebe7d7929574ab0ce4b3386db8a7893a
tenztshering <tenztshering3@gmail.com>
2023-04-07 10:28:38 +0600

    :sparkles: enhanced

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
+const optimizedImages = require('next-optimized-images');
+const withPlugins = require('next-compose-plugins');
+
 const nextConfig = {
   experimental: {
     appDir: false,
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPlugins([optimizedImages], nextConfig);
