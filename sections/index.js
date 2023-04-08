428768e39f7a45b6e49114c1542f49df39701f7d
tenztshering <tenztshering3@gmail.com>
2023-04-09 01:02:51 +0600

    :sparkles: bug fixes

--- sections/index.js
+++ sections/index.js
@@ -1,10 +1,9 @@
-export { default as Hero } from "./Hero";
-export { default as About } from "./About";
-export { default as Features } from "./Features";
-export { default as Subscriptions } from "./Subscriptions";
-export { default as Brands } from "./Brands";
-export { default as Feedback } from "./Contact";
-export { default as AppFeatures } from "./AppFeatures";
+export { default as Hero } from './Hero';
+export { default as About } from './About';
+export { default as Subscriptions } from './Subscriptions';
+export { default as Brands } from './Brands';
+export { default as Feedback } from './Contact';
+export { default as AppFeatures } from './AppFeatures';
 
 //Legal
-export { default as LegalDocs } from "./LegalDocs";
+export { default as LegalDocs } from './LegalDocs';
